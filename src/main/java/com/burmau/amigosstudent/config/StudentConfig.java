7a9df58756e5c9b8b80a165056d777ba90f622a2
Burmau <burmaud1@gmail.com>
2023-11-30 02:18:08 -0500

    started documentation

---
+++ src/main/java/com/burmau/amigosstudent/config/StudentConfig.java
@@ -0,0 +1,22 @@
+package com.burmau.amigosstudent.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class StudentConfig {
+    @Bean
+    public OpenAPI customOpenAPI(@Value("${app.description}") String appDescription, @Value("${app.version}") String appVersion){
+        return new OpenAPI()
+                .info(new Info().title("Student Display API")
+                .version(appVersion)
+                .description(appDescription)
+                .termsOfService("htt://swagger.io/terms.")
+                .license(new License().name("Apache 2.0")
+                        .url("http://springdoc.org")));
+    }
+}
--- src/main/java/com/burmau/amigosstudent/controller/StudentController.java
+++ src/main/java/com/burmau/amigosstudent/controller/StudentController.java
@@ -2,6 +2,9 @@ package com.burmau.amigosstudent.controller;
 
 import com.burmau.amigosstudent.model.Student;
 import com.burmau.amigosstudent.service.StudentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -10,27 +13,31 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("api/v1/")
 @CrossOrigin(value = "http://localhost:3000/")
+@Tag(name = "Student Controller", description = "This REST controller provides services to manages the Student web application.")
 @AllArgsConstructor
 public class StudentController {
     private final StudentService studentService;
-
     @GetMapping
     @ResponseStatus(code = HttpStatus.OK)
+    @Operation(summary = "Provides all the student objects in the database snapshot", method = "GET")
     public Iterable<Student> allStudents(){
         return studentService.studentList();
     }
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
-    public void addStudent(@RequestBody Student student){
+    @Operation(summary = "Performs the duty of adding a new student to the database", method = "POST")
+    public void addStudent(@Valid @RequestBody Student student){
         studentService.add(student);
     }
-    @DeleteMapping("/{student_id}")
-    @ResponseStatus(code = HttpStatus.ACCEPTED)
+    @DeleteMapping("/{student_id}") 
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    @Operation(summary = "Performs the duty of deleting a student with the given id from the database", method = "DELETE")
     public void deleteStudent(@PathVariable int student_id){
         studentService.delete(student_id);
     }
     @PutMapping("/{student_id}")
-    @ResponseStatus(code = HttpStatus.OK)
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    @Operation(summary = "Performs the duty of updating a student object with the given id and object into the database", method = "PUT")
     public void edit(@RequestBody Student student, @PathVariable int student_id){
         studentService.update(student, student_id);
     }
